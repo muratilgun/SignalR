5e1f97d0614970617969d92f28831a1df17ba62f
muratilgun <muratilgun34@gmail.com>
2021-09-25 20:47:14 +0300

    overrided OnConnected & OnDisconnectedAsync methods
    implemented Context.ConnectId

--- SignalRExample/Hubs/MyHub.cs
+++ SignalRExample/Hubs/MyHub.cs
@@ -8,9 +8,22 @@ namespace SignalRExample.Hubs
 {
     public class MyHub : Hub
     {
+        static List<string> clients = new List<string>();
         public async Task SendMessage(string message)
         {
             await Clients.All.SendAsync("receiveMessage", message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userJoined", Context.ConnectionId);         
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            clients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userLeft", Context.ConnectionId);
         }
     }
 }
