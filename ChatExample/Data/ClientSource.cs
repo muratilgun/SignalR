a392d96cf3e5e0cfa9aba5535aa173e623dd5db9
muratilgun <muratilgun34@gmail.com>
2021-09-28 00:08:09 +0300

    Chat App
    I started coding the chat application. I do not apply n-layered architecture as the project is kept at a simple level. I made a simple frontend similar to the previous one. I am using the jquery library. I also implemented a few bootstrap components.

---
+++ ChatExample/Data/ClientSource.cs
@@ -0,0 +1,13 @@
+﻿using ChatExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatExample.Data
+{
+    public static class ClientSource
+    {
+        public static List<Client> Clients { get; } = new List<Client>();
+    }
+}
---
+++ ChatExample/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using ChatExample.Data;
+using ChatExample.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatExample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task GetNickName(string nickName)
+        {
+            Client client = new Client
+            {
+                ConnectionId = Context.ConnectionId,
+                NickName = nickName
+            };
+            ClientSource.Clients.Add(client);
+            await Clients.Others.SendAsync("clientJoined", nickName);
+
+        }
+    }
+}
